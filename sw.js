86d58d1ec63649388be5deca885b278fc4e597cf
Hung Pham <hungvbhpcba6@gmial.com>
2026-08-02 12:36:59 +0700

    Fix lỗi sync: thêm ?v=1 cho api-config.js (tránh SW/browser giữ bản cũ URL localhost), bump SW cache v14

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Chiến lược: network-first cho điều hướng, stale-while-revalidate cho tĩnh. */
 'use strict';
 
-const CACHE = 'taskflow-v13';
+const CACHE = 'taskflow-v14';
 const APP_SHELL = [
   './',
   './index.html',
